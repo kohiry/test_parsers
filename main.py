ca6bc40da356c6005f3554e7c9dbccfec6a12220
kohiry <kohiry@mail.ru>
2023-08-29 23:03:34 +0400

    upd readme

--- main.py
+++ main.py
@@ -8,12 +8,26 @@ from abc import ABC, abstractmethod
 import asyncio
 
 
+class AppList:
+    apps = []
+
+    @classmethod
+    def add_class(cls, klass):
+        cls.apps.append(klass)
+
+
+def save_app(klass):
+    AppList.add_class(klass)
+    return klass
+
+
 class App(ABC):
     @abstractmethod
     def run(self) -> None:
         pass
 
 
+@save_app
 class SynchronApp(App):
     def __init__(self):
         self.folder_name: str = "data"
@@ -22,23 +36,34 @@ class SynchronApp(App):
         download_all_photos(SynchronPhotoDownloader(), self.folder_name)
 
 
+@save_app
 class AsynchronApp(App):
     def __init__(self):
         self.folder_name: str = "data"
 
     async def run(self):
-        download_all_photos(AsynchronPhotoDownloader(), self.folder_name)
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(
+            download_all_photos(AsynchronPhotoDownloader(), self.folder_name)
+        )
 
 
 @timer
 def main(task: str):
     if not task.isalnum():
         print("Bad value.")
+        return 400
     task = int(task)
-    if task == 1:
-        SynchronApp().run()
-    elif task == 2:
-        asyncion.run(AsynchronApp().run())
+    if len(AppList.apps) < task:
+        print("Not a option.")
+        return 404
+    i = 0
+    for app in AppList.apps:
+        if i + 1 == task:
+            print("Ok.")
+            app().run()
+            return 200
+        i += 1
 
 
 if __name__ == "__main__":
