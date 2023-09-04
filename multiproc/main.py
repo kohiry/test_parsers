a6a2693271c5c2b4b9f30fb466b9d5ccf31ed98f
kohiry <kohiry@mail.ru>
2023-09-04 09:27:53 +0400

    update

---
+++ multiproc/main.py
@@ -0,0 +1,34 @@
+"""Этот модуль содержит класс App, который представляет собой приложение.
+
+Класс App предоставляет методы для запуска приложения.
+"""
+from parser import download_all_photos
+from decorator import timer
+
+
+class App:
+    """App.
+
+    Представляет приложение.
+    """
+
+    def __init__(self):
+        """Метод инициализации с главной папкой."""
+        self.folder_name = "data"
+
+    def run(self):
+        """Функция run.
+
+        Запускает приложение.
+        """
+        download_all_photos(self.folder_name)
+
+
+@timer
+def main():
+    """Основная функция для запуска приложения."""
+    App.run()
+
+
+if __name__ == "__main__":
+    main()
